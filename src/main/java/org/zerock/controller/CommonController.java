67c27b7a8b0a6149cfc99a12ddb589612fa6c6aa
dd2558 <dd2558@naver.com>
2024-03-19 10:11:36 +0900

    CommonController 메소드추가

--- src/main/java/org/zerock/controller/CommonController.java
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -13,9 +13,24 @@ public class CommonController {
 
 	@GetMapping("/accessError")
 	public void accessDenied(Authentication auth, Model model) {
-		log.info("access Denied: " + auth);
+		log.info("access Denied : " + auth);
 		
 		model.addAttribute("msg","Access Denied");
+	}
+
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model){
+		
+		log.info("error: " + error);
+		log.info("logout: " + logout);
+		
+		if(error !=null) {
+			model.addAttribute("error","Login Error Check Your Account");
+		}
+		
+		if(logout !=null) {
+			model.addAttribute("logout","Logout!");
+		}
 	}
 	
 }
--- src/main/java/org/zerock/security/CustomAccessDeniedHandler.java
+++ src/main/java/org/zerock/security/CustomAccessDeniedHandler.java
@@ -12,7 +12,8 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {@Override
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
 		log.error("Access Denied Handler");
